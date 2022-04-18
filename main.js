4671b1c625a44cad597a0990f97155c426c0fe38
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-04-18 10:58:39 +0700

    add main.js accordion

---
+++ main.js
@@ -0,0 +1,8 @@
+const dropdown = document.querySelectorAll(".dropdown");
+dropdown.forEach((item,index) => item.addEventListener('click',function(){
+    console.log(index);
+    this.classList.toggle("pane-active")
+    var p = document.querySelector(`#pane${index}`)
+    console.log(p);
+    p.classList.toggle("pane-show")
+}) )
