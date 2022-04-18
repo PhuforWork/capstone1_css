608c71a4526be504e9a44eb89298365ca5c0a226
PhuocMinh111 <phuocminhlenguyen@gmail.com>
2022-04-18 20:07:40 +0700

    countUp js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+import { CountUp } from './countup.js/dist/countUp.js';
+const demo1 = new CountUp('counter1', 10586, {enableScrollSpy:true});
+const demo2 = new CountUp('counter2', 860, {enableScrollSpy:true});
+const demo3 = new CountUp('counter3', 4394, {enableScrollSpy:true});
+demo1.start()
+demo2.start()
+demo3.start()
 const dropdown = document.querySelectorAll(".dropdown");
 dropdown.forEach((item,index) => item.addEventListener('click',function(){
     console.log(index);
