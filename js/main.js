a4174a0f8f71b43251bb12a3351e168ae002afe3
quocphu1997 <congaquevip@gmail.com>
2022-05-01 16:15:06 +0700

    add btn dark mode and theme dark-mode

--- js/main.js
+++ js/main.js
@@ -84,5 +84,25 @@ input5.onclick = function () {
   }
 };  
 
+// toggle dark mode
+window.onload = function () {
+  const themeBtn = document.getElementById('_1st-toggle-btn');
+  themeBtn.addEventListener('click', function () {
+    // Lấy thuộc tính data-theme
+    const root = document.querySelector(':root');
+    const isLightMode =
+      root.getAttribute('data-theme') === 'dark' ? false : true;
+    // toggle theme mode
+    if (isLightMode) {
+      root.setAttribute('data-theme', 'dark');
+    } else {
+      root.setAttribute('data-theme', 'light');
+    }
+    // thay đổi vị trí của button
+    this.classList.toggle('active');
+  });
+};
+// 
+
 
 
